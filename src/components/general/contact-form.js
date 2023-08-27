4f008c50b5fca304165cf0466476914f026b1b69
Febrian Syahroni <febrian.fs716@gmail.com>
2023-08-27 19:03:02 +0700

    update

--- src/components/general/contact-form.js
+++ src/components/general/contact-form.js
@@ -1,8 +1,12 @@
-import React from 'react'; // Import React untuk komponen React
-import { useState } from 'react'; // Import useState untuk mengelola state komponen
+import React, { useState, useRef } from 'react';
 
 export default function ContactForm() {
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwqGFxJ0D9CJT0Sc2v1udvYJ2PTsv1PFGxLJ7AuQO3gtjibNpv8i_wk6_ZNA60pTPdo/exec'; // Ganti dengan URL skrip yang benar
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwqGFxJ0D9CJT0Sc2v1udvYJ2PTsv1PFGxLJ7AuQO3gtjibNpv8i_wk6_ZNA60pTPdo/exec';
+
+  const btnKirimRef = useRef(null);
+  const btnLoadingRef = useRef(null);
+  const myAlertRef = useRef(null);
+
   const [formData, setFormData] = useState({
     nama: '',
     email: '',
@@ -11,9 +15,14 @@ export default function ContactForm() {
 
   const handleSubmit = e => {
     e.preventDefault();
+    btnLoadingRef.current.classList.toggle("d-none");
+    btnKirimRef.current.classList.toggle("d-none");
     fetch(scriptURL, { method: 'POST', body: new FormData(e.target) })
       .then(response => {
         if (response.ok) {
+          btnLoadingRef.current.classList.toggle("d-none");
+          btnKirimRef.current.classList.toggle("d-none");
+          myAlertRef.current.classList.remove("d-none");
           console.log('Form berhasil dikirim ke Google Sheet');
         } else {
           console.error('Terjadi kesalahan saat mengirim formulir');
@@ -34,13 +43,13 @@ export default function ContactForm() {
 
   return (
     <>
-      <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-        <button type="button" className="btn-close border text-white" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div>
       <form name="submit-to-google-sheet" onSubmit={handleSubmit}>
-        <div className="w-full lg:w-2/3 lg:mx-auto">
+        <div className="w-full lg:w-2/3 lg:mx-auto">      
           <div className="w-full px-4 mb-8">
+            <div className="d-none my-alert alert alert-success alert-dismissible fade show" role="alert">
+              <strong>Terima kasih!</strong> Pesan anda sudah terkirim.
+              <button className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>              
             <label className="text-base font-bold text-dark" htmlFor="nama">Nama</label>
             <input
               name="nama"
@@ -66,8 +75,8 @@ export default function ContactForm() {
           </div>
           {/* Bagian lain dari formulir (email, pesan) serupa */}
           <div className="w-full px-4">
-            <button type="submit" className="text-base font-semibold bg-kedua py-3 px-8 rounded-full text-white w-full hover:opacity-80 hover:shadow-lg transition duration-500">Kirim</button>
-            <button className="d-none text-base font-semibold bg-emerald-400 py-3 px-8 rounded-full text-white w-full" type="button" disabled>
+            <button type="submit" className="btn-kirim text-base font-semibold bg-kedua py-3 px-8 rounded-full text-white w-full hover:opacity-80 hover:shadow-lg transition duration-500">Kirim</button>
+            <button className="d-none btn-loading text-base font-semibold bg-emerald-400 py-3 px-8 rounded-full text-white w-full" type="button" disabled>
               <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
               <span role="status">Loading...</span>
             </button>
